fd188c918bf960077c4add66a29bc28850c999cc
Salam4nder <kamran.personal@icloud.com>
2024-07-07 20:14:39 +0200

    feat: don't overthink it?

--- time.go
+++ time.go
@@ -1,84 +1,64 @@
 package main
 
-import "time"
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"time"
+)
 
-type Week struct {
-	Monday    time.Duration `json:"monday"`
-	Tuesday   time.Duration `json:"tuesday"`
-	Wednesday time.Duration `json:"wednesday"`
-	Thursday  time.Duration `json:"thursday"`
-	Friday    time.Duration `json:"friday"`
-	Staruday  time.Duration `json:"saturday"`
-	Sunday    time.Duration `json:"sunday"`
+type Day struct {
+	Date time.Time
+	Val  time.Duration
 }
 
-func (x *Week) OverwriteIfExist(w time.Weekday, d time.Duration) {
-	switch w {
-	case time.Monday:
-		if x.Monday != 0 {
-			x.Monday += d
-			return
-		}
-		x.Monday = d
-	case time.Tuesday:
-		if x.Tuesday != 0 {
-			x.Tuesday += d
-			return
-		}
-		x.Tuesday = d
-	case time.Wednesday:
-		if x.Wednesday != 0 {
-			x.Wednesday += d
-			return
-		}
-		x.Wednesday = d
-	case time.Thursday:
-		if x.Thursday != 0 {
-			x.Thursday += d
-			return
-		}
-		x.Thursday = d
-	case time.Friday:
-		if x.Friday != 0 {
-			x.Friday += d
-			return
-		}
-		x.Friday = d
-	case time.Saturday:
-		if x.Staruday != 0 {
-			x.Staruday += d
-			return
-		}
-		x.Staruday = d
-	case time.Sunday:
-		if x.Staruday != 0 {
-			x.Staruday += d
-			return
-		}
-		x.Sunday = d
-	default:
-		return
+type Interval struct {
+	Days []*Day
+}
+
+func DecodeInterval(b []byte) (*Interval, error) {
+	var i *Interval
+	if err := gob.NewDecoder(bytes.NewReader(b)).Decode(i); err != nil {
+		return nil, err
 	}
+	return i, nil
 }
 
-type Month struct {
-	Weeks []*Week `json:"weeks"`
+func (x *Interval) Encode() ([]byte, error) {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(x); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
-func (x *Month) LastWeek() *Week {
-	if len(x.Weeks) == 0 {
+func (x *Interval) LastDay() *Day {
+	if x == nil || len(x.Days) == 0 {
 		return nil
 	}
-	return x.Weeks[len(x.Weeks)-1]
+	return x.Days[len(x.Days)-1]
 }
 
-type Year struct {
-	Months []*Month `json:"months"`
-}
-
-func (x *Year) LastMonth() *Month {
-	if len(x.Months) == 0 {
-		return nil
+func (x *Interval) NewIfEmpty() *Interval {
+	if x == nil || len(x.Days) == 0 {
+		x = new(Interval)
+		x.Days = append(x.Days, &Day{})
+		return x
 	}
-	return x.Months[len(x.Months)-1]
+	return x
+}
+
+func (x *Interval) Print() {
+	if x == nil || len(x.Days) < 1 {
+		fmt.Println("deepwork: no recorded days yet, run `deepwork` to start a timer!")
+	}
+	for _, d := range x.Days {
+		fmt.Println(
+			d.Date.Year(),
+			d.Date.Month(),
+			d.Date.Weekday(),
+			d.Val.Round(time.Hour),
+			" in hours",
+		)
+	}
 }
