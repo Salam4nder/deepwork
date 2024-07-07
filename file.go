1ad964eb7dcc43118c9cc6384dd1b0d8cf3b6c12
Salam4nder <kamran.personal@icloud.com>
2024-07-07 14:02:22 +0200

    feat: opening file

--- file.go
+++ file.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
@@ -10,23 +11,46 @@ const (
 	whereErrorOccurred = "creating initial file: "
 )
 
+func filePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", errors.New("filepath: getting home dir")
+	}
+	return fmt.Sprintf("%s/%s", homeDir, fileName), nil
+}
+
+// openFile will open the config file and return it.
+// Caller is responsible to close it.
+func openFile() (*os.File, error) {
+	path, err := filePath()
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("open file: %w", err)
+	}
+
+	return f, nil
+}
+
 func createInitialFile(off bool) error {
 	if off {
 		fmt.Println("create file off, doing nothing")
 		return nil
 	}
 
-	homeDir, err := os.UserHomeDir()
+	path, err := filePath()
 	if err != nil {
-		fmt.Println(whereErrorOccurred + "getting user home dir")
+		return err
 	}
-	filePath := fmt.Sprintf("%s/%s", homeDir, fileName)
 
-	f, err := os.Open(filePath)
+	f, err := os.Open(path)
 	if err != nil {
 		if os.IsNotExist(err) {
 			fmt.Fprintf(os.Stdout, "creating initial file in %s", filePath)
-			cF, err := os.Create(filePath)
+			cF, err := os.Create(path)
 			if err != nil {
 				return fmt.Errorf("%s, %w", fmt.Sprint(whereErrorOccurred+"creating file"), err)
 			}
